86d99a8c3a80db9825b2d4492378404924f52fa6
manasabell <manasabell@gmail.com>
2020-05-14 16:35:02 +0000

    ddl file creation

---
+++ linux_sql/scripts/host_info.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+# CLI arguments set to local variables
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+#hardware specifications from various commands formatted.
+#lscpu_out=`lscpu`
+#save hostname to a variable
+hostname=$(hostname -f)
+#save number of cpu's to a variable
+cpu_number=$(lscpu  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+#echo "$cpu_number"
+#save the architecture of cpu to a variable
+cpu_architecture=$(lscpu | egrep "^Architecture:" | awk '{print $2}' | xargs)
+#to capture the model of the cpu
+cpu_model=$(lscpu | egrep "^Model name:" | awk '{print $3 $4 $5 $6 $7}' |xargs)
+#to capture the frequence of the cpu
+cpu_mhz=$(lscpu | egrep "^CPU MHz:" | awk '{print $3 $4}' |xargs)
+# to capture the cache details of cpu
+l2_cache=$(lscpu | egrep "^L2 cache:" | awk '{print $3}' |xargs|egrep -o '[0-9]+')
+#to fetch the total memory in the cpu
+total_mem=$(cat /proc/meminfo | egrep "^MemTotal:" | awk '{print $2}' |xargs)
+#to fetch the time stamp of the current transaction
+timestamp=$(vmstat -t | awk '{print $18 " " $19}'|xargs)
+
+# -h specifies the hostname of the machine on which the server is running
+# -U specifies the username
+# -d specifies the name of the database to connect to
+# -c specified that we are running single command
+# -p specifies the port number on which the server is running
+psql -h "$psql_host" -U "$psql_user" -d "$db_name" -p "$psql_port" "$psql_password" -c "INSERT INTO host_info(
+                                                   hostname, cpu_number, cpu_architecture,
+                                                  cpu_model, cpu_mhz, l2_cache, total_mem,
+                                                    timestamp
+                                                  ) VALUES
+  ('"$hostname" + !', "$cpu_number", '"$cpu_architecture"', '"$cpu_model"', "$cpu_mhz","$l2_cache","$total_mem",'"$timestamp"');"
+
+#to execute the script in terminal
+#./scripts/host_info.sh "localhost" 5432 "host_agent" "postgres" "password"
+#./scripts/host_info.sh psql_host psql_port db_name psql_user psql_password
+
+
+
+
+
+
+
---
+++ linux_sql/sql/ddl.sql
@@ -0,0 +1,36 @@
+--list the databases
+--\l;
+
+-- attach to the database host_agent;
+\c host_agent;
+
+--check the existing databases
+\dt;
+
+-- DDL statement for host_info table creation
+CREATE TABLE IF NOT EXISTS PUBLIC.host_info (
+  id SERIAL         NOT NULL,
+  hostname          VARCHAR NOT NULL,
+  cpu_number        SMALLINT NOT NULL,
+  cpu_architecture  VARCHAR NOT NULL,
+  cpu_model         VARCHAR NOT NULL,
+  cpu_mhz           FLOAT(4) NOT NULL,
+  L2_cache          SMALLINT NOT NULL,
+  total_mem         INTEGER NOT NULL,
+  timestamp         TIMESTAMP NOT NULL,
+  CONSTRAINT pk_hostname PRIMARY KEY(hostname,id)
+);
+
+-- DDL statement for host_usage table creation
+CREATE TABLE IF NOT EXISTS PUBLIC.host_usage (
+  host_id       SERIAL NOT NULL PRIMARY KEY,
+  "timestamp"   TIMESTAMP NOT NULL,
+  memory_free   INTEGER NOT NULL,
+  cpu_idle      INTEGER NOT NULL,
+  cpu_kernel    INTEGER NOT NULL,
+  disk_io       INTEGER NOT NULL,
+  disk_available INTEGER NOT NULL
+  );
+
+  --check for the newly created tables
+  \dt;
